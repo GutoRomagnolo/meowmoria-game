b6d8cef90d1b38ebd2ae267742cbef070a84ba18
GutoRomagnolo <guromagnolo23@hotmail.com>
2022-10-30 01:39:48 -0300

    feat: adicionando todas as fotos das cartas

---
+++ src/JogoPadrão/jogo_padrao.js
@@ -0,0 +1,32 @@
+const boardContainer = document.getElementById('container-board');
+
+const resolveBoardSize = chosenSize => {
+  switch (chosenSize) {
+    case '2x2':
+      generateBoardGame(4);
+      break;
+    case '4x4':
+      generateBoardGame(16);
+      break;
+    case '8x8':
+      generateBoardGame(64);
+      break;
+    default:
+      backToMenu();
+  }
+
+  boardContainer.classList.add(`container-tabuleiro-${chosenSize}`);
+}
+
+
+const generateBoardGame = boardSize => {
+  for (let index = 0; index < boardSize; index++) {
+    const boardCard = document.createElement('img');
+    boardCard.src='./../assets/ImagensTemporarias/GragasSquare.webp'
+    boardCard.classList.add('board-cell');
+
+    boardContainer.appendChild(boardCard)
+  }
+}
+
+resolveBoardSize('8x8')
--- src/SelecionarModo/selecionarmodo.js
+++ src/SelecionarModo/selecionarmodo.js
@@ -1,32 +1,32 @@
 const selectedStyle = `
-    border: 0.313rem dashed #07ff07;
-    box-shadow: none;
+  border: 0.313rem dashed #07ff07;
+  box-shadow: none;
 `;
 
 var modo_jogo = '';
 
 function selectClassico() {
-    document.getElementById('card-classico').style.cssText = selectedStyle;
-    document.getElementById('card-tempo').style = null;
-    modo_jogo = 'classico';
+  document.getElementById('card-classico').style.cssText = selectedStyle;
+  document.getElementById('card-tempo').style = null;
+  modo_jogo = 'classico';
 }
 
 function selectTempo() {
-    document.getElementById('card-tempo').style.cssText = selectedStyle;
-    document.getElementById('card-classico').style = null;
-    modo_jogo = 'contra_tempo';
+  document.getElementById('card-tempo').style.cssText = selectedStyle;
+  document.getElementById('card-classico').style = null;
+  modo_jogo = 'contra_tempo';
 }
- 
+
 function jogar() {
-    var tamanho = document.getElementById("seletor-tabuleiro").value;
-    localStorage.setItem("tamanho", tamanho);
-    if(modo_jogo == 'classico') {
-        window.open("./../JogoPadrão/jogo_padrao.html", "_self")
-    }
-    else if(modo_jogo == 'contra_tempo') {
-        window.open("./../JogoContraTempo/jogo_contra_tempo.html", "_self")
-    }
-    else {
-        document.getElementById('erro').style.visibility = 'visible';
-    }
+  var tamanho = document.getElementById("seletor-tabuleiro").value;
+  localStorage.setItem("tamanho", tamanho);
+  if (modo_jogo == 'classico') {
+    window.open("./../JogoPadrão/jogo_padrao.html", "_self")
+  }
+  else if (modo_jogo == 'contra_tempo') {
+    window.open("./../JogoContraTempo/jogo_contra_tempo.html", "_self")
+  }
+  else {
+    document.getElementById('erro').style.visibility = 'visible';
+  }
 }
